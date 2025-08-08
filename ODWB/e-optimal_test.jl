97ba5eee29af2a54f67428a11ac47e4fa42d6d05
Deborah Hendrych <d.hendrych@web.de>
2025-08-08 14:48:35 +0200

    Adapt interface.

--- ODWB/e-optimal_test.jl
+++ ODWB/e-optimal_test.jl
@@ -1,8 +1,9 @@
 using ODWB
 using Boscia
+using FrankWolfe
 
 seed = 1
-m = 100
+m = 50
 n = Int(floor(sqrt(m)))
 corr = false
 
@@ -13,7 +14,9 @@ f, generate_smoothing_function = ODWB.build_e_criterion(A)
 
 x, _, result = Boscia.solve(f, nothing, lmo; 
 mode = Boscia.SMOOTHING_MODE,
-settings_bnb = Boscia.settings_bnb(verbose=true, print_iter=10),
-settings_smoothing = Boscia.settings_smoothing(mode=Boscia.SMOOTHING_MODE, generate_smoothing_objective = generate_smoothing_function),
-settings_frank_wolfe = Boscia.settings_frank_wolfe(mode=Boscia.SMOOTHING_MODE, max_fw_iter=1000),
+settings_bnb = Boscia.settings_bnb(verbose=true, print_iter=100, time_limit=60),
+settings_smoothing = Boscia.settings_smoothing(mode=Boscia.SMOOTHING_MODE, generate_smoothing_objective = generate_smoothing_function),## , smoothing_start=5.0, smoothing_min=1.0
+settings_frank_wolfe = Boscia.settings_frank_wolfe(mode=Boscia.SMOOTHING_MODE, max_fw_iter=1000, fw_verbose=false, line_search=FrankWolfe.Adaptive()),
 )
+
+x, result = ODWB.solve_opt(seed, m, n, 300, "E", corr, full_callback=false, write=false, verbose=true)
