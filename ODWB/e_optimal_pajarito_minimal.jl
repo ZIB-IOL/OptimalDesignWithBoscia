1a919f9c7b27827f5070b11277670a1602ee85db
Deborah Hendrych <d.hendrych@web.de>
2025-08-20 10:25:04 +0200

    Clean MWE for issue report.

--- ODWB/e_optimal_pajarito_minimal.jl
+++ ODWB/e_optimal_pajarito_minimal.jl
@@ -4,39 +4,17 @@
 using JuMP
 using Pajarito
 using HiGHS
-using SCIP
 using Hypatia
 using LinearAlgebra
-using Random
-using Distributions
-using StableRNGs
-using Clarabel
 
 A = [0.640560125688334 0.8988924651705463 0.6026315293750658 0.09942414283811873; 0.3244420648938249 0.21960977858670605 0.8752427026103645 0.7330721267274701; 0.4284151183931746 0.06037385523635341 0.4479437123418444 0.40800090600302463; 0.3539981006442484 0.11431932418843094 0.6797418029178991 0.3676010970291791; 0.6240687276682546 0.8276760414998834 0.8951460038632557 0.9408622568595247; 0.6093405087159935 0.5475397233252607 0.3753804665727667 0.6283323077663837; 0.8148945670877987 0.8159334877118128 0.42139733688904235 0.4596363701822491; 0.8581761761029205 0.6791489434158131 0.8162784435702374 0.38054865157982964; 0.4037354612337182 0.9234073469913137 0.4925105187447588 0.4202196080406787; 0.0465632242904217 0.588608026827289 0.21073940556559967 0.9357195644138552; 0.46079375823300184 0.9655435124426841 0.33129739228282684 0.8749097390632607; 0.23653413851585414 0.9689717890441853 0.4285687872702951 0.9887116589634328; 0.5616823570640757 0.3700511410277585 0.7705341197413721 0.8850442312936958; 0.8295347567225702 0.9252731672804814 0.11200245309187484 0.654354549533481; 0.6877230841636806 0.36562522511027085 0.5380654173827643 0.33035378144723415; 0.48457722652079827 0.15673193660803542 0.20485906499945394 0.6362942989719393; 0.8574183603562568 0.5761171392212233 0.6599616466199261 0.5833842527846491; 0.5019688218144165 0.4815792427281611 0.5051499066121654 0.7951315576134046; 0.46509258174600643 0.8499021885218158 0.6089184177322454 0.1776254689978578; 0.9139243738764997 0.1512127891717162 0.8357702479228285 0.25057409400489294]
-
 N = 6
 
-# E-criterion function (adapted from utilities.jl)
-function build_e_criterion(A)
-    m, n = size(A)
-    
-    function inf_matrix(x)
-        return Symmetric(A' * diagm(x) * A)
-    end
-
-    function f(x)
-        X = inf_matrix(x)   
-        return (-1) * minimum(eigvals(X))    
-    end
-
-    return f
-end
-
 # Build E-optimal Pajarito model (based on build_E_pajarito_model)
-function build_E_pajarito_model_minimal(m, n, time_limit; verbose=true, zero_one=false)
+function build_E_pajarito_model_minimal(m, n, time_limit; verbose=true)
     # Generate data
     #A, N, ub = build_data(seed, m, n, zero_one=zero_one)
-    ub = zero_one ? fill(1.0, m) : rand(1.0:N/3, m)
+    ub = fill(1.0, m)
     
     println("Problem size: m=$m, n=$n, N=$N, sum(ub)=$(sum(ub))")
 
@@ -96,12 +74,12 @@ function build_E_pajarito_model_minimal(m, n, time_limit; verbose=true, zero_one
 end
 
 # Main solving function
-function solve_e_optimal_pajarito_minimal(;m=50, n=7, time_limit=300, verbose=true, zero_one=false)
+function solve_e_optimal_pajarito_minimal(;m=50, n=7, time_limit=300, verbose=true)
     println("=== E-Optimal Design with Pajarito ===")
     println("Parameters: m=$m, n=$n")
     
     # Build model
-    model, x, t, A, N, ub = build_E_pajarito_model_minimal(m, n, time_limit, verbose=verbose, zero_one=zero_one)
+    model, x, t, A, N, ub = build_E_pajarito_model_minimal(m, n, time_limit, verbose=verbose)
     
     # Solve the model
     println("\nSolving...")
@@ -127,18 +105,6 @@ function solve_e_optimal_pajarito_minimal(;m=50, n=7, time_limit=300, verbose=tr
     println("Solution x: $(solution_x[1:min(20, length(solution_x))])")
     println("Sum of x: $(sum(solution_x))")
     
-    # Check feasibility
-    println("\n=== Feasibility Check ===")
-    println("Sum constraint satisfied: $(abs(sum(solution_x) - N) < 1e-6)")
-    println("Non-negativity satisfied: $(all(solution_x .>= -1e-6))")
-    println("Upper bound satisfied: $(all(solution_x .<= ub .+ 1e-6))")
-    
-    # Compute actual E-criterion value
-    f = build_e_criterion(A)
-    actual_e_value = f(solution_x)
-    println("Actual E-criterion value: $(-actual_e_value)")
-    println("Theoretical lower bound (t): $(solution_t)")
-    
     return solution_x, solution_t, status, solve_time_sec
 end
 
@@ -151,7 +117,6 @@ if abspath(PROGRAM_FILE) == @__FILE__
         n=size(A, 2),      # n (number of parameters)  
         time_limit=300,    # time_limit
         verbose=true,   # verbose
-        zero_one=true    # zero_one
     )
     
     println("\n=== Compare with a different solver ===")
