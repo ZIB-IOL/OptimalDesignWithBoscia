9db291a3eb6e51a53854e21cb2da2ed4c78d69c2
Hendrych <dhendryc@htc-login001.zib.de>
2026-02-25 15:40:40 +0100

    minor changes.

--- ODWB/opt_design_script.jl
+++ ODWB/opt_design_script.jl
@@ -10,13 +10,13 @@
 #solvers = ["Boscia", "Pajarito", "SCIP", "Custom", "SOCP"]
 
 # E-Opimal Design
-num_experiments = [50,80,100,120,150]
+num_experiments = [50, 100, 150, 200]#[50,80,100,120,150,170,200]
 criteria = ["E"] # "EF"
 data_types = ["IND", "CORR"]
 integer_data = [false]  #[0, 1]
-solvers = ["Boscia", "SCIPSDP"] #, "Pajarito"
-seeds = [0] #[1,2,3,4,5]
-options = ["baseline", "use_exclusion_criterion", "oa"]
+solvers = ["Boscia"] #, "Pajarito" , "SCIPSDP"
+seeds = [1,2,3] #[1,2,3,4,5]
+options = ["mu_testing"] #["baseline", "use_exclusion_criterion", "oa", "mu_testing"]
 N_construct = ["one", "log", "rank_deficient"] 
 
 
@@ -30,10 +30,10 @@ for criterion in criteria
                         for int_data in integer_data
                             for N in N_construct
                                 # 
-                                if (option == "use_exclusion_criterion" && solver == "SCIPSDP") || (option == "oa" && solver == "Boscia")
+                                if (option == "use_exclusion_criterion" && solver == "SCIPSDP") || (option == "oa" && solver == "Boscia") || (option == "mu_testing" && solver == "SCIPSDP")
                                     continue
                                 end
-                                run(`sbatch -A optimi -J E-Opt experiment.sbatch $criterion $solver $data $m $int_data $seed $option $N`) # CB
+                                run(`sbatch -A optimi -J E-B-M experiment.sbatch $criterion $solver $data $m $int_data $seed $option $N`) # CB
                             end
                         end
                     end
