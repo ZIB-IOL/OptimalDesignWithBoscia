54f9cf0ec2bb051d164dd6d165205683771d3d22
Deborah Hendrych <d.hendrych@web.de>
2026-03-19 16:34:10 +0100

    Dual exclusion.

--- ODWB/opt_design_script.jl
+++ ODWB/opt_design_script.jl
@@ -16,7 +16,7 @@ criteria = ["E"] # "EF"
 data_types = ["IND", "CORR"]
 solvers = ["Boscia"] #, "Pajarito" , "SCIPSDP"
 seeds = [0] #[1,2,3,4,5]
-options = ["exclusion_criterion", "exclusion_criterion_random", "exclusion_criterion_tighter_tol"] #["baseline", "use_exclusion_criterion", "oa", "mu_testing"]
+options = ["dual_exclusion_criterion"] #["baseline", "use_exclusion_criterion", "oa", "mu_testing"]
 N_construct = ["one", "log"] #["one", "log", "rank_deficient"] 
 
 # Connectivity 
