b574649829e64d79dff2a3688b5a3ac5cf133c17
Deborah Hendrych <d.hendrych@web.de>
2026-03-26 15:35:49 +0100

    Fix.

--- ODWB/run_optimal_design.jl
+++ ODWB/run_optimal_design.jl
@@ -133,7 +133,7 @@ for k in ratio_para
                             ls_secant = false,
                             best_sol_by_original = option in ["best_sol", "best_sol_resolve_integer"],
                             resolve_integer_solution = option in ["resolve_integer", "best_sol_resolve_integer"],
-                            sub_grad_info = option != "no_sub_grad_info",
+                            use_sub_grad_info = option != "no_sub_grad_info",
                             )
                     elseif mode == "SCIP"
                         if criterion in ["A", "D", "E", "EF"]
