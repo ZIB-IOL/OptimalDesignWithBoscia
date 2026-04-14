02b5dcace8fb92174d1905e69d7172adc67764b6
Hendrych <dhendryc@htc-login001.zib.de>
2026-04-14 22:44:07 +0200

    Reduced spectrum runs.

--- ODWB/opt_design_script.jl
+++ ODWB/opt_design_script.jl
@@ -10,15 +10,15 @@
 #solvers = ["Boscia", "Pajarito", "SCIP", "Custom", "SOCP"]
 
 # E-Opimal Design
-#=
-num_experiments = [50,80,100,120,150] #170,200]#[50,80,100,120,150,170,200]  # for AGC [80, 100, 150, 200]
+
+num_experiments = [50,80,100,120,150, 170, 200] #170,200]#[50,80,100,120,150,170,200]  # for AGC [80, 100, 150, 200]
 criteria = ["E"] # "EF"
 data_types = ["IND", "CORR"]
 solvers = ["Boscia"] #, "Pajarito" , "SCIPSDP"
 seeds = [1,2,3,4,5] #[1,2,3,4,5]
-options = ["eigenvalue_based_pruning"] #["baseline", "use_exclusion_criterion", "oa", "mu_testing"]
+options = ["reduced_spectrum"] #["baseline", "use_exclusion_criterion", "oa", "mu_testing"]
 N_construct = ["one", "log"] #["one", "log", "rank_deficient"] 
-=#
+
 # Connectivity 
 #=
 num_experiments = [80, 100, 150, 200]
@@ -26,20 +26,20 @@ criteria = ["AGC"]
 data_types = ["IND","CORR"]
 solvers = ["Boscia"]
 seeds = [1,2,3,4,5]
-options = ["eigenvalue_based_pruning"]
+options = ["reduced_spectrum"]
 N_construct = ["one"] 
 =#
 
 # Connectivity spanning trees
-
+#=
 num_experiments = [10, 12, 15, 25, 40, 60, 100]
-criteria = ["ACST"]
+criteria = ["ACST", "ACSTS"]
 data_types = ["IND"]
-solvers = ["SCIPSDP_bnb", "SCIPSDP_oa"]
-seeds = [0]
-options = ["baseline"]
+solvers = ["Boscia"]
+seeds = [1,2,3]
+options = ["reduced_spectrum"]
 N_construct = ["one"] 
-
+=#
 # create instances
 for criterion in criteria
     for data in data_types
@@ -52,7 +52,7 @@ for criterion in criteria
                             if (option == "exclusion_criterion" && solver == "SCIPSDP") || (option == "oa" && solver == "Boscia")
                                 continue
                             end
-                            run(`sbatch -A optimi -J S-ACST experiment.sbatch $criterion $solver $data $m $seed $option $N`) # CB
+                            run(`sbatch -A optimi -J B-E experiment.sbatch $criterion $solver $data $m $seed $option $N`) # CB
                         end
                     end
                 end
