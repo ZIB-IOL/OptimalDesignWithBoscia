f43d4ccacc6e8a8f9b892fce376b33663fe1fe68
Deborah Hendrych <d.hendrych@web.de>
2026-04-14 12:17:31 +0200

    Some testing.

---
+++ ODWB/reduced_spectrum.jl
@@ -0,0 +1,33 @@
+using ODWB
+using LinearAlgebra
+
+for m in 50:20:200
+    n = Int(floor(m/10))
+    @show m, n
+    N = Int(floor(1.5 * n))
+    A, _, N, _ = ODWB.build_data(1, m, n, false, false)
+    eigs = reverse(eigvals(-A'*A))
+    max_sing = svdvals(-A' * A)[1]
+    mu_start = m/100
+    mu_decay = 0.7
+    mu_min = exp10(-100/m)
+    for depth in 1:min(m, 100)
+        μ = max(mu_start * (mu_decay)^(depth - 1), mu_min)
+        epsilon = max(1e-2 * 0.8^(depth - 1), 1e-6)
+        delta = epsilon/6
+        @show μ, epsilon, delta
+        for k in 1:n
+            lhs = (sqrt(2) * (n - k) * exp((eigs[k] - eigs[1])/μ))/(sum(exp((eigs[j] - eigs[1])/μ) for j in 1:k))
+            rhs = delta/max_sing
+            if !isfinite(lhs)
+                @show k, eigs[k], eigs[1], μ
+            end
+            #@show k, lhs, rhs
+            if lhs <= rhs
+                @show k, lhs, rhs
+                break
+            end
+        end 
+    end
+    println("\n")
+end
