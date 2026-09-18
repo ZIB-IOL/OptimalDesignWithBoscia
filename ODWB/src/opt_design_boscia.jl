f13ad599d25842082c37cdbc4dc6d20565d2e79e
Deborah Hendrych <d.hendrych@web.de>
2026-09-18 16:32:17 +0200

    Bonobo no longer a dependency of boscia.

--- ODWB/src/opt_design_boscia.jl
+++ ODWB/src/opt_design_boscia.jl
@@ -460,7 +460,7 @@ function solve_opt(
         settings.branch_and_bound[:time_limit] = 10
         settings.branch_and_bound[:use_shadow_set] = use_shadow_set
         settings.branch_and_bound[:branching_strategy] = branching_strategy
-        settings.branch_and_bound[:traverse_strategy] = depthfirstsearch ? Boscia.BiasedDepthFirstSearch(true) : Bonobo.BestFirstSearch()
+        settings.branch_and_bound[:traverse_strategy] = depthfirstsearch ? Boscia.BiasedDepthFirstSearch(true) : Boscia.BestFirstSearch()
 
         settings.tolerances[:rel_dual_gap] = relative_gap_tolerance
         settings.tolerances[:dual_gap] = N < n ? 1e-4 : 1e-6
